4de404c3528bf7bf8622cb7fc5f9f4cf59a86e65
tomdaniels <thomsondaniels@hotmail.com>
2019-04-11 16:46:45 +1000

    higher order functions

---
+++ hof.js
@@ -0,0 +1,21 @@
+// Higher order function
+
+// 1. accepts a function as an argument
+// 2. returns a new function
+
+const withCount = fn => {
+  let count = 0;
+
+  return (...args) => {
+    console.log(`call count: ${++count}`);
+    return fn(...args)
+  }
+}
+
+const add = (x, y) => x + y;
+
+const countedAdd = withCount(add);
+
+console.log(countedAdd(1, 2))
+console.log(countedAdd(2, 2))
+console.log(countedAdd(3, 2))
