d1f1459c2a18f22fa01bf8ff43e74b55fe76b483
tomdaniels <thomsondaniels@hotmail.com>
2019-04-13 13:23:43 +1000

    composition

---
+++ comp.js
@@ -0,0 +1,30 @@
+// Composition
+
+const d = x => x + 2
+const e = x => x * 3
+
+// console.log(f(g(5))) 17
+
+// Composition
+
+const f = x => x + 2
+const g = x => x * 3
+
+const scream = str => str.toUpperCase()
+const exclaim = str => `${str}!`
+const repeat = str => `${str} ${str}`
+
+// console.log(
+//   repeat(exclaim(scream('I love egghead')))
+// )
+
+const compose = (...fns) => x =>
+  fns.reduceRight((acc, fn) => fn(acc), x);
+
+const withExuberance = compose(
+  repeat, 
+  exclaim, 
+  scream
+);
+
+// console.log(withExuberance('nice socks, man'));
