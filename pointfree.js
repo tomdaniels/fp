a4ebda17f1832bfc7f263b4d7a1615f47788e4a6
tomdaniels <thomsondaniels@hotmail.com>
2019-04-13 13:05:35 +1000

    point free programming

---
+++ pointfree.js
@@ -0,0 +1,9 @@
+// Point free programming
+
+const array = [1, 2, 3];
+const doube = x => x * 2;
+array.map(double);
+
+// Legibility 
+// Reduce surface area for bugs 
+// unit test our named functions
