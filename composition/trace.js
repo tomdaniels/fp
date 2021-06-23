4f3155618af5fc60b7f68ee82c30d99187e082c3
Tom Daniels <thomsondaniels@hotmail.com>
2021-06-23 17:43:12 +1000

    trace func to debug compositions

---
+++ composition/trace.js
@@ -0,0 +1,26 @@
+/**
+ * composition offers readable code, but can come at the cost of difficult debugging.
+ * a composable logger can splip into any composition  
+ */
+
+const trace = label => value => {
+  console.log(label, value);
+  return value;
+}
+
+const compose = (...fns) => args =>
+  // pass the accumulating value to each function recieved
+  fns.reduce((previous, item) => item(previous), args);
+
+const increment = val => val+1;
+const dubble = val => val*2;
+
+const envokeWith = compose(
+  trace('before increment :'),
+  increment,
+  trace('before double: '),
+  dubble,
+  trace('after: ')
+);
+
+envokeWith(5);
