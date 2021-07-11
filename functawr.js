895d6f56998f3e5eda7dd057059cf4e330c328d9
Tommy Daniels <thomsondaniels@hotmail.com>
2021-07-11 17:30:33 +1000

    same same

--- functawr.js
+++ functawr.js
@@ -10,19 +10,19 @@ const trace = x => {
 // any datatype is now mappable like an array..
 const i = Identity(2);
 
-// Identity law
+// Identity law - same same
 const r1 = i;
 const r2 = i.map(x => x);
 
-r1.map(trace);
-r2.map(trace);
+r1.map(trace); // 2
+r2.map(trace); // 2
 
 const f = n => n + 1;
 const g = n => n * 2;
 
-// Composition law
+// Composition law - same same
 const r3 = i.map(x => f(g(x)));
 const r4 = i.map(g).map(f);
 
-r3.map(trace);
-r4.map(trace);
+r3.map(trace); // 5
+r4.map(trace); // 5
